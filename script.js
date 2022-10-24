132a832c906e3a0ed0f984ec158edf948e574320
michaelkebernik <michaelkebernik@yahoo.de>
2022-10-24 15:40:32 +0200

    Seite formatiert

---
+++ script.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Tom Dill",
+        "answer_2": "Tim Bernes-Lee",
+        "answer_3": "Jason Hiller",
+        "answer_4": "Xio-Mee",
+        "right_answer": 2
+    },  {
+        "question": "Welches dieser HTML-Tags erstellt einen Absatz?",
+        "answer_1": "<p>",
+        "answer_2": "<h1>",
+        "answer_3": "<button>",
+        "answer_4": "<b>",
+        "right_answer": 1
+    },  {
+        "question": "Was ist HTML?",
+        "answer_1": "Ein Computer",
+        "answer_2": "Ist ein Computerspiel",
+        "answer_3": "Eine Software",
+        "answer_4": "Die Sprache des Internets",
+        "right_answer": 2
+    },  {
+        "question": "Was ist ein Attribut?",
+        "answer_1": "src",
+        "answer_2": "img",
+        "answer_3": "a",
+        "answer_4": "index",
+        "right_answer": 1
+    },  {
+        "question": "Was ist kein Strukturelement?",
+        "answer_1": "header",
+        "answer_2": "body",
+        "answer_3": "main",
+        "answer_4": "Visual Studio",
+        "right_answer": 4
+    },  {
+        "question": "Was ist ein Link?",
+        "answer_1": "<ul>",
+        "answer_2": "<tr>",
+        "answer_3": "<a href=..>",
+        "answer_4": "<overflow-x>",
+        "right_answer": 3
+    },  {
+        "question": "Wer passt hier nicht in die Reihe?",
+        "answer_1": "ActionScript",
+        "answer_2": "LiveScript",
+        "answer_3": "JavaScript",
+        "answer_4": "AppleScript",
+        "right_answer": 4
+    },
+]
